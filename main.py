46bfc33f4f67d9844e6544824ca76f7da37b6c21
YaseenStu <111099331+MuhammadYaseen11@users.noreply.github.com>
2025-04-16 01:13:29 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 
-import pandas as pd
+from sklearn.model_selection import train_test_split
 
-data = pd.read_excel("sales_data.xlsx")
 
-print(data["Product ID"].value_counts())
+# import pandas as pd
+
+# data = pd.read_excel("sales_data.xlsx")
+
+# data['Discount Applied'] = data['Discount Applied'].map({'Yes': 1, 'No': 0})
+
+# print(data)
